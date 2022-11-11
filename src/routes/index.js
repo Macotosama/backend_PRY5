552b15ef0273ee0234148bf3f86294990e1b3bcd
DESKTOP-0SDNMEI\Usuario <anthony00052023@gmail.com>
2022-11-11 15:09:10 -0600

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,28 @@
 const {Router} = require('express');
 const router = Router();
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL:'https://node-firebase-d105d-default-rtdb.firebaseio.com/'
+});
+
+const db = admin.database();
 
 router.get('/',(req,res)=>{
     console.log('Index szzzzz');
+    res.send('recived');
+});
+
+
+router.post('/guardarinfojugadoresr',(req,res)=>{
+    const newJugador ={
+        nombre: req.body.nombre,
+        mejorTiempo: req.body.mejorTiempo
+
+    };
+    console.log(req.body);
+    db.ref('Jugadores').push(newJugador);
     res.send('recived');
 });
 module.exports = router;
