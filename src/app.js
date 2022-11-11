8d78121f5e4b221c5e031a90c4ec6ad38fe1fde9
DESKTOP-0SDNMEI\Usuario <anthony00052023@gmail.com>
2022-11-11 17:09:58 -0600

    commit

--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const path = require('path');
+var bodyParser = require('body-parser');
 
 const app = express();
 //settings
 app.set('port',process.env.PORT || 4000);
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
+app.use(bodyParser.json());
 //midelwares
 app.use(morgan('dev'));
 app.use(express.urlencoded({
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,20 +11,21 @@ admin.initializeApp({
 
 const db = admin.database();
 
-router.get('/',(req,res)=>{
+router.get('/inicio',(req,res)=>{
     console.log('Index szzzzz');
     res.send('recived');
 });
 
 
-router.post('/guardarinfojugadoresr',(req,res)=>{
+router.post('/guardarinfojugadores',(req,res)=>{
+    console.log(req.body);
     const newJugador ={
         nombre: req.body.nombre,
         mejorTiempo: req.body.mejorTiempo
-
     };
     console.log(req.body);
     db.ref('Jugadores').push(newJugador);
+
     res.send('recived');
 });
 module.exports = router;
